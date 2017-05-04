855f33d64d351e7880a60d9b5a516103e4c4f023
emmanuel <emmanuel-lionet@homail.fr>
2017-05-04 14:38:09 +0200

    :)

--- src/saboteur/model/OperationPathCard.java
+++ src/saboteur/model/OperationPathCard.java
@@ -1,5 +1,13 @@
 package saboteur.model;
 
 public class OperationPathCard extends Operation {
+	private Position p;
+	
+	public Position getP() {
+		return p;
+	}
 
+	public OperationPathCard(Position pos){
+		p = pos;
+	}
 }
