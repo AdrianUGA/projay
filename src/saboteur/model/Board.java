05823b9121dae18cfe1963e8de81411ec384fa27
adrian <adrian.amaglioa@etu.univ-grenoble-alpes.fr>
2017-05-04 14:45:48 +0200

    :)

--- src/saboteur/model/Board.java
+++ src/saboteur/model/Board.java
@@ -3,9 +3,18 @@ package saboteur.model;
 import saboteur.model.Card.*;
 
 public class Board {
-	private Graphe graph;
+	private PathCard[][] board;
 	
-	public void addCard(PathCard card){
-		//TODO
+	public Board(){
+		this.board = new PathCard[89][89];
 	}
+	
+	public void addCard(PathCard card, Position position){
+		this.board[position.getcX()][position.getcY()] = card;
+	}
+	
+	public void removeCard(Position position){
+		this.board[position.getcX()][position.getcY()] = null;
+	}
+	
 }
