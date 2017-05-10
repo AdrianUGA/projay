4e0232b91f4d1b105d7f0980def51c57d90e6b16
RO EX <RO EX>
2017-05-10 15:23:39 +0200

    method isPossible modified

--- src/saboteur/model/Board.java
+++ src/saboteur/model/Board.java
@@ -156,24 +156,37 @@ public class Board {
 
 	public boolean isPossible(PathCard card, Position position){
 		PathCard neighbor;
+		boolean atLeastOnePath = false;
     	
 		//Test North
 		neighbor = this.getCard(new Position(position.getcX(), position.getcY()-1));
-		if (neighbor != null && (card.isOpen(Cardinal.NORTH)^neighbor.isOpen(Cardinal.SOUTH))) return false;
+		if (neighbor != null){
+			if (card.isOpen(Cardinal.NORTH) && neighbor.isOpen(Cardinal.SOUTH)) atLeastOnePath = true;
+			if (card.isOpen(Cardinal.NORTH)^neighbor.isOpen(Cardinal.SOUTH)) return false;
+		}
 		
 		//Test East
 		neighbor = this.getCard(new Position(position.getcX()+1, position.getcY()));
-		if (neighbor != null && (card.isOpen(Cardinal.EAST)^neighbor.isOpen(Cardinal.WEST))) return false;
+		if (neighbor != null){
+			if (card.isOpen(Cardinal.EAST) && neighbor.isOpen(Cardinal.WEST)) atLeastOnePath = true;
+			if (card.isOpen(Cardinal.EAST)^neighbor.isOpen(Cardinal.WEST)) return false;
+		}
 		
 		//Test South
 		neighbor = this.getCard(new Position(position.getcX(), position.getcY()+1));
-		if (neighbor != null && (card.isOpen(Cardinal.SOUTH)^neighbor.isOpen(Cardinal.NORTH))) return false;
+		if (neighbor != null){
+			if (card.isOpen(Cardinal.SOUTH) && neighbor.isOpen(Cardinal.NORTH)) atLeastOnePath = true;
+			if (card.isOpen(Cardinal.SOUTH)^neighbor.isOpen(Cardinal.NORTH)) return false;
+		}
 		
 		//Test West
 		neighbor = this.getCard(new Position(position.getcX()-1, position.getcY()));
-		if (neighbor != null && (card.isOpen(Cardinal.WEST)^neighbor.isOpen(Cardinal.EAST))) return false;
+		if (neighbor != null){
+			if (card.isOpen(Cardinal.WEST) && neighbor.isOpen(Cardinal.EAST)) atLeastOnePath = true;
+			if (card.isOpen(Cardinal.WEST)^neighbor.isOpen(Cardinal.EAST)) return false;
+		}
 		
-		return true;
+		return (card.isGoal() || card.isStart() || atLeastOnePath);
 	}
 	
 	public int indice(Position pos){
