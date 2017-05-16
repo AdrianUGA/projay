e1805da7d6f0e5a6f91d7f29b0d22dd1f15dca97
Anthony LAFONT <anthony.lafont@outlook.com>
2017-05-16 12:57:28 +0200

    update state machine to stack state machine

--- src/saboteur/state/GameState.java
+++ src/saboteur/state/GameState.java
@@ -48,6 +48,8 @@ public class GameState implements State{
     private Game game;
     private Stage primaryStage;
 
+    private boolean pause;
+
     public GameState(GameStateMachine gsm, Game game, Stage primaryStage){
         this.gsm = gsm;
         this.game = game;
@@ -58,7 +60,7 @@ public class GameState implements State{
     public void update() {
         if (this.game.gameIsFinished()){
             //fin de la partie
-        	if(game.dwarfsWon())System.out.println("Nains ont gagné");
+            if(game.dwarfsWon())System.out.println("Nains ont gagné");
             //this.gsm.change("annonce vainqueur");
             //System.out.println("fin de partie");
         } else {
@@ -72,15 +74,16 @@ public class GameState implements State{
                     this.game.getCurrentPlayer().playCard();
                     this.game.getCurrentPlayer().pickCard();
                     //System.out.println("AI has played");
-                    /*try{
-                        Thread.sleep(3000);
-                    } catch (Exception e){
-                        e.printStackTrace();
-                    }*/
+                /*try{
+                    Thread.sleep(3000);
+                } catch (Exception e){
+                    e.printStackTrace();
+                }*/
                     this.game.nextPlayer();
                 }
             }
         }
+        System.out.println("coucou");
     }
 
     @Override
@@ -90,111 +93,117 @@ public class GameState implements State{
 
     @Override
     public void onEnter(Object param) {
-    	/*
+    	initGame();
+    }
+    
+    @Override
+    public void onExit() {
+
+    }
+
+    private void initGame(){
+        /*
     	 * Pour Emmanuel qui fait des tests avec les IA
     	 * Pour pas être embêter par le problème de setTeam(),
     	 * commente son appel dans la méthode newRound de la classe Game
     	 */
-    	//Début du bloc à commenter
-    	
-    	this.game.getPlayerList().clear();
-    	this.game.addPlayer(new AI(this.game, "Yves"));
-    	this.game.addPlayer(new AI(this.game, "Philippe"));
-    	this.game.addPlayer(new AI(this.game, "Jean-Marie"));
-		
-    	//Fin du bloc à commenter
-    	
+        //Début du bloc à commenter
+
+        this.game.getPlayerList().clear();
+        this.game.addPlayer(new AI(this.game, "Yves"));
+        this.game.addPlayer(new AI(this.game, "Philippe"));
+        this.game.addPlayer(new AI(this.game, "Jean-Marie"));
+
+        //Fin du bloc à commenter
+
         this.game.newGame();
-        
+
+        this.pause = false;
+
         Resources resources = new Resources();
         resources.loadImage();
         resources.loadPicto();
         HashMap<String, Image> allCards = resources.getImageCard();
-        
+
         for(Player p : this.game.getPlayerList()){
-    		if(p.isAI()){
-    			((AI)p).initializeAI();
-    		}
-    	}
-        
+            if(p.isAI()){
+                ((AI)p).initializeAI();
+            }
+        }
+
         try{
             FXMLLoader loader = new FXMLLoader();
             loader.setLocation(App.class.getResource("/saboteur/view/boardGame.fxml"));
             loader.setController(this);
             Pane rootLayout = loader.load();
             Scene scene = new Scene(rootLayout);
-            
+
             //Take size of screen
             Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
             double gameTableSize = primaryScreenBounds.getHeight();
             double gameTableHalfSize = gameTableSize/2;
             double boardSize = gameTableHalfSize/2;
-            
+
             //For center cards
             this.cardContainer.setPrefWidth(gameTableSize);
-            
+
             //The game board
             GridPane p = new GridPane();
-            
+
             double cardWidth = 108/3;
             double cardHeight = 166/3;
-			for(int i = 0; i < Board.getGridSize(); i++) {
-				for (int j = 0; j < Board.getGridSize(); j++) {
-					ImageView img = new ImageView();
-            		if(i>30 && i<40 && j>30 && j<40) {
-            			img.setFitHeight(cardHeight);
-    					img.setFitWidth(cardWidth);
-            			img.setImage(allCards.get("broken_cart_card.png"));
-            		}
-            		p.add(img, i, j);
-            	}
+            for(int i = 0; i < Board.getGridSize(); i++) {
+                for (int j = 0; j < Board.getGridSize(); j++) {
+                    ImageView img = new ImageView();
+                    if(i>30 && i<40 && j>30 && j<40) {
+                        img.setFitHeight(cardHeight);
+                        img.setFitWidth(cardWidth);
+                        img.setImage(allCards.get("broken_cart_card.png"));
+                    }
+                    p.add(img, i, j);
+                }
             }
-			
-			double innerRadius = gameTableHalfSize/2;
-	        double radians = Math.toRadians(135);
-	        double XstartInner = (int)Math.round((Math.cos(radians) * innerRadius + gameTableHalfSize));
-	        double YstartInner = (int)Math.round((Math.sin(-radians) * innerRadius + gameTableHalfSize));
+
+            double innerRadius = gameTableHalfSize/2;
+            double radians = Math.toRadians(135);
+            double XstartInner = (int)Math.round((Math.cos(radians) * innerRadius + gameTableHalfSize));
+            double YstartInner = (int)Math.round((Math.sin(-radians) * innerRadius + gameTableHalfSize));
             p.setLayoutX(XstartInner);
             p.setLayoutY(YstartInner);
-			
+
             this.boardContainer.getChildren().add(p);
-            
+
             //Create Specific arc for first player
 //            currentPlayerArcAndMiniCircle(gameTableHalfSize, gameTableHalfSize, boardSize);
-        	addPlayerOnTheBoard(gameTableHalfSize, gameTableHalfSize, 100, -140.0);
-            
+            addPlayerOnTheBoard(gameTableHalfSize, gameTableHalfSize, 100, -140.0);
+
             //Create arc for other player
             int nbPlayer = this.game.getPlayerList().size()-1;
             double length = 260.0 / nbPlayer; // 260 = 360 - 100 (100 degree of the 1st player)
             double startAngle = -140.0;
             for (int i = 0; i<nbPlayer ; i++) {
-            	startAngle = startAngle - length;
-            	addPlayerOnTheBoard(gameTableHalfSize, gameTableHalfSize, length, startAngle);
+                startAngle = startAngle - length;
+                addPlayerOnTheBoard(gameTableHalfSize, gameTableHalfSize, length, startAngle);
             }
 
             //Create the circle of the game board
             this.gameBoard.setCenterX(gameTableHalfSize);
             this.gameBoard.setCenterY(gameTableHalfSize);
             this.gameBoard.setRadius(boardSize);
-            
+
             this.primaryStage.setScene(scene);
             this.primaryStage.setFullScreen(true);
             this.primaryStage.setFullScreenExitKeyCombination(KeyCombination.NO_MATCH);
         } catch (IOException e){
             e.printStackTrace();
         }
-        
+
         this.firstCard.setImage(allCards.get("broken_cart_card.png"));
         this.secondCard.setImage(allCards.get("collapse_card.png"));
         this.thirdCard.setImage(allCards.get("path_card_17.png"));
         this.fourthCard.setImage(allCards.get("path_card_29.png"));
         this.fifthCard.setImage(allCards.get("path_card_40.png"));
         this.sixthCard.setImage(allCards.get("plan_card.png"));
-    }
-    
-    @Override
-    public void onExit() {
-
     }
     
     //End of turn of the current player.
@@ -213,7 +222,8 @@ public class GameState implements State{
     
     @FXML
     private void optionsButtonAction(){
-    	System.out.println("options");
+        this.pause = true;
+        this.gsm.push("pauseMenu");
     }
     
     @FXML
