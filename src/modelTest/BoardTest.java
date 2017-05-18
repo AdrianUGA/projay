3b0b0b2f734e3bdde018a541c60be46fe840c97e
RobinExtrant <robin.extrant@gmail.com>
2017-05-18 10:09:01 +0200

    maj testIsPossible

--- src/modelTest/BoardTest.java
+++ src/modelTest/BoardTest.java
@@ -14,7 +14,7 @@ public class BoardTest {
 
 	//NOT SUCCESS CURRENTLY : Due to board's constructor
 	@Test
-	public void testIsPossible() {/*
+	public void testIsPossible() {
 		int xStart = Board.START.getcX();
 		int yStart = Board.START.getcY();
 		String cross[] = {"NORTH", "EAST", "SOUTH", "WEST"};
@@ -25,8 +25,12 @@ public class BoardTest {
 		PathCard crossCard = new PathCard(cross, false, false, false, false);
 		PathCard horizontalBarCard = new PathCard(horizontalBar, false, false, false, false);
 		PathCard verticalBarCard = new PathCard(verticalBar, false, false, false, false);
-		PathCard angleRightCard = new PathCard(angleRight, false, false, false, false);
-		PathCard angleLeftCard = new PathCard(angleLeft, false, false, false, false);
+		PathCard angleRightCardSouth = new PathCard(angleRight, false, false, false, false);
+		PathCard angleLeftCardSouth = new PathCard(angleLeft, false, false, false, false);
+		PathCard angleLeftCardNorth = angleRightCardSouth.clone();
+		angleLeftCardNorth.reverse();
+		PathCard angleRigthCardNorth = angleLeftCardSouth.clone();
+		angleRigthCardNorth.reverse();
 		
 		PathCard start = new PathCard(cross, false, true, false, false);
 		PathCard goalWithGold = new PathCard(cross, false, false, true, true);
@@ -58,28 +62,28 @@ public class BoardTest {
 		assertEquals(false, boardTest.isPossible(verticalBarCard, new Position(xStart+1,yStart+1)));
 		assertEquals(false, boardTest.isPossible(horizontalBarCard, new Position(xStart-1,yStart+1)));
 		
-		assertEquals(true, boardTest.isPossible(angleRightCard, new Position(xStart-1,yStart-1)));
-		assertEquals(true, boardTest.isPossible(angleLeftCard, new Position(xStart+1,yStart-1)));
-		assertEquals(true, boardTest.isPossible(angleRightCard.reversed(), new Position(xStart+1,yStart+1)));
-		assertEquals(true, boardTest.isPossible(angleLeftCard.reversed(), new Position(xStart-1,yStart+1)));
-		boardTest.addCard(angleRightCard, new Position(xStart-1,yStart-1));
-		boardTest.addCard(angleLeftCard, new Position(xStart+1,yStart-1));
-		boardTest.addCard(angleRightCard.reversed(), new Position(xStart+1,yStart+1));
-		boardTest.addCard(angleLeftCard.reversed(), new Position(xStart-1,yStart+1));
+		assertEquals(true, boardTest.isPossible(angleRightCardSouth, new Position(xStart-1,yStart-1)));
+		assertEquals(true, boardTest.isPossible(angleLeftCardSouth, new Position(xStart+1,yStart-1)));
+		assertEquals(true, boardTest.isPossible(angleLeftCardNorth, new Position(xStart+1,yStart+1)));
+		assertEquals(true, boardTest.isPossible(angleRigthCardNorth, new Position(xStart-1,yStart+1)));
+		boardTest.addCard(angleRightCardSouth, new Position(xStart-1,yStart-1));
+		boardTest.addCard(angleLeftCardSouth, new Position(xStart+1,yStart-1));
+		boardTest.addCard(angleLeftCardNorth, new Position(xStart+1,yStart+1));
+		boardTest.addCard(angleRigthCardNorth, new Position(xStart-1,yStart+1));
 		
-		assertEquals(true, boardTest.isPossible(angleRightCard, new Position(xStart,yStart-2)));
-		assertEquals(true, boardTest.isPossible(angleLeftCard, new Position(xStart+2,yStart)));
-		assertEquals(true, boardTest.isPossible(angleRightCard.reversed(), new Position(xStart,yStart+2)));
-		assertEquals(true, boardTest.isPossible(angleLeftCard.reversed(), new Position(xStart-2,yStart)));
-		boardTest.addCard(angleRightCard, new Position(xStart,yStart-2));
-		boardTest.addCard(angleLeftCard, new Position(xStart+2,yStart));
-		boardTest.addCard(angleRightCard.reversed(), new Position(xStart,yStart+2));
-		boardTest.addCard(angleLeftCard.reversed(), new Position(xStart-2,yStart));
+		assertEquals(true, boardTest.isPossible(angleRightCardSouth, new Position(xStart,yStart-2)));
+		assertEquals(true, boardTest.isPossible(angleLeftCardSouth, new Position(xStart+2,yStart)));
+		assertEquals(true, boardTest.isPossible(angleLeftCardNorth, new Position(xStart,yStart+2)));
+		assertEquals(true, boardTest.isPossible(angleRigthCardNorth, new Position(xStart-2,yStart)));
+		boardTest.addCard(angleRightCardSouth, new Position(xStart,yStart-2));
+		boardTest.addCard(angleLeftCardSouth, new Position(xStart+2,yStart));
+		boardTest.addCard(angleLeftCardNorth, new Position(xStart,yStart+2));
+		boardTest.addCard(angleRigthCardNorth, new Position(xStart-2,yStart));
 		
-		assertEquals(false, boardTest.isPossible(angleLeftCard, new Position(xStart+1,yStart-2)));
-		assertEquals(false, boardTest.isPossible(angleRightCard.reversed(), new Position(xStart+2,yStart+1)));
-		assertEquals(false, boardTest.isPossible(angleLeftCard.reversed(), new Position(xStart-1,yStart+2)));
-		assertEquals(false, boardTest.isPossible(angleRightCard, new Position(xStart-2,yStart-1)));
+		assertEquals(false, boardTest.isPossible(angleLeftCardSouth, new Position(xStart+1,yStart-2)));
+		assertEquals(false, boardTest.isPossible(angleLeftCardNorth, new Position(xStart+2,yStart+1)));
+		assertEquals(false, boardTest.isPossible(angleRigthCardNorth, new Position(xStart-1,yStart+2)));
+		assertEquals(false, boardTest.isPossible(angleRightCardSouth, new Position(xStart-2,yStart-1)));
 		
 		assertEquals(true, boardTest.isPossible(horizontalBarCard, new Position(xStart+1,yStart-2)));
 		assertEquals(true, boardTest.isPossible(verticalBarCard, new Position(xStart+2,yStart+1)));
@@ -88,7 +92,7 @@ public class BoardTest {
 		
 		assertEquals(false, boardTest.isPossible(horizontalBarCard, new Position(xStart+Board.DISTANCE_START_OBJECTIVE_X-1,yStart)));
 		goal.setVisible(true);
-		assertEquals(true, boardTest.isPossible(horizontalBarCard, new Position(xStart+Board.DISTANCE_START_OBJECTIVE_X-1,yStart)));*/
+		assertEquals(true, boardTest.isPossible(horizontalBarCard, new Position(xStart+Board.DISTANCE_START_OBJECTIVE_X-1,yStart)));
 	}
 
 }
